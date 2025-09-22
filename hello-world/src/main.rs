5ef5a4571c8993fe89c668072bc8cd3fd904a2d3
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2025-09-22 09:49:17 +0200

    add notes

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,10 @@
+// Run using compiler directly
+// compile: rustc main.rs
+// run:     ./main.rs
+//
+// Run using Cargo
+// compile: cargo build
+// run:     ./target/debug/hello-world
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!"); // the ! is present because it is a macros not a function
 }
