6b5ab6074c4526a64c9de928e0018c77cd110948
Tomas Petricek <tomaspetricek98@gmail.com>
2025-09-22 09:53:32 +0200

    add notes

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,10 +1,13 @@
-// Run using compiler directly
+// # Run using compiler directly
 // compile: rustc main.rs
 // run:     ./main.rs
 //
-// Run using Cargo
+// # Run using Cargo
+// ## Two Steps
 // compile: cargo build
 // run:     ./target/debug/hello-world
+// ## Single Step
+// cargo run
 fn main() {
     println!("Hello, world!"); // the ! is present because it is a macros not a function
 }
