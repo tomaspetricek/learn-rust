9996279d35d9b3795d8b0813bf4ea95f5ca8c13b
Tomas Petricek <tomaspetricek98@gmail.com>
2025-10-03 10:22:45 +0200

    use modules

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,64 @@
+use std::fmt::Result;
+use std::io::Result as IoResult;
+use std::io::{self, Write}
+
+use std::{cmp::Ordering, io}; // nested paths
+use std::collections::*; // glob operator
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+// use crate::front_of_house::hosting;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    hosting::add_to_waitlist();
+}
