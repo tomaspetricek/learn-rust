279ad9752a90c9d707f3b1f1e7daccfa1f258daf
Tomas Petricek <tomaspetricek98@gmail.com>
2025-10-10 09:55:18 +0200

    learn collections

--- use_collections/src/main.rs
+++ use_collections/src/main.rs
@@ -1,6 +1,131 @@
 use std::collections::HashMap;
 
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
 fn main() {
     let mut map = HashMap::new();
     map.insert(1, 2);
+
+    let v: Vec<i32> = Vec::new();
+    let mut v = vec![1, 2, 3];
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let third = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+
+    if let Some(&value) = third {
+        println!("The third element is {value}");
+    }
+    let v = vec![100, 32, 57];
+
+    for i in &v {
+        println!("{i}");
+    }
+    let mut v = vec![100, 32, 57];
+
+    for i in &mut v {
+        *i += 50; // dereferecing
+    }
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    // strings
+    let mut s = String::new();
+
+    let data = "initial contents";
+
+    let s = data.to_string();
+
+    let s = "initial contents".to_string();
+
+    let s = String::from("initial contents");
+
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שלום");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+
+    let mut s = String::from("lo");
+    s.push('l');
+
+    // concatenation
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // s1 gets moved
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{s1}-{s2}-{s3}"); // call does not take owneship of any of the parameters
+    println!("result of concatention: {s}");
+
+    let hello = String::from("Здравствуйте");
+
+    for c in "Зд".chars() {
+        println!("{c}");
+    }
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }
+
+    // hash maps
+    let mut scores = HashMap::new();
+    scores.insert(String::from("blue"), 10);
+    scores.insert(String::from("yellow"), 50);
+
+    let team_mate = String::from("blue");
+    let score = scores.get(&team_mate).copied().unwrap_or(0);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("blue"), 10);
+    scores.insert(String::from("blue"), 20);
+    println!("{scores:?}");
+
+    // insert only if key does not yet exist
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("{scores:?}");
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() { // returns iterator over subslices
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{map:?}");
 }
