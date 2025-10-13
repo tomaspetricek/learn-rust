93db3f5f7464d2341ad3cac30dabe2dfc365a7fe
Tomas Petricek <tomaspetricek98@gmail.com>
2025-10-13 14:05:18 +0200

    solve merge sorted arrays

--- solve-problems/src/main.rs
+++ solve-problems/src/main.rs
@@ -95,6 +95,33 @@ impl Solution {
     pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
         Solution::solve_sudoku_impl(board, 0, 0);
     }
+
+    pub fn merge_sorted_arrays(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        assert_eq!((m + n) as usize, nums1.len());
+
+        if nums1.len() == 0 {
+            return;
+        }
+        let mut fst_len = m as usize;
+        let mut snd_len = n as usize;
+        let mut res_idx = nums1.len() - 1;
+
+        while fst_len != 0 && snd_len != 0 {
+            if nums1[fst_len - 1] > nums2[snd_len - 1] {
+                nums1[res_idx] = nums1[fst_len - 1];
+                fst_len -= 1;
+            } else {
+                nums1[res_idx] = nums2[snd_len - 1];
+                snd_len -= 1;
+            }
+            res_idx -= 1;
+        }
+        if snd_len != 0 {
+            for i in 0..snd_len {
+                nums1[i] = nums2[i];
+            }
+        }
+    }
 }
 
 fn main() {
@@ -129,4 +156,19 @@ fn main() {
         vec!['3', '4', '5', '2', '8', '6', '1', '7', '9'],
     ];
     assert_eq!(board, expect_board);
+
+    let mut nums1 = vec![1, 2, 3, 0, 0, 0];
+    let mut nums2 = vec![2, 5, 6];
+    Solution::merge_sorted_arrays(&mut nums1, 3, &mut nums2, 3);
+    assert_eq!(vec![1, 2, 2, 3, 5, 6], nums1);
+
+    let mut nums1 = vec![0];
+    let mut nums2 = vec![1];
+    Solution::merge_sorted_arrays(&mut nums1, 0, &mut nums2, 1);
+    assert_eq!(vec![1], nums1);
+
+    let mut nums1 = vec![1];
+    let mut nums2 = vec![];
+    Solution::merge_sorted_arrays(&mut nums1, 1, &mut nums2, 0);
+    assert_eq!(vec![1], nums1);
 }
