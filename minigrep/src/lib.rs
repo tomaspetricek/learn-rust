1db132bc27ca017ec1c51ba5fbddfcf77655ef84
Tomas Petricek <tomaspetricek98@gmail.com>
2025-10-16 09:53:22 +0200

    write minigrep

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(&query, &contents));
+    }
+}
---
+++ minigrep/src/main.rs
@@ -0,0 +1,46 @@
+use minigrep::search;
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
+fn main() {
+    // read command line arguments
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|error| {
+        println!("problem parsing arguments: {error}");
+        process::exit(1);
+    });
+    dbg!(&args);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Self { query, file_path })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
