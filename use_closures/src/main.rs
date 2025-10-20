960b1e92a3035bcd13ba1fd4874b5283a894ef8b
Tomas Petricek <tomaspetricek98@gmail.com>
2025-10-20 10:43:39 +0200

    use closures

---
+++ use_closures/src/main.rs
@@ -0,0 +1,120 @@
+use std::thread;
+use std::time::Duration;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked()) // self is captured as imutable reference
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+fn add_one_v1(x: u32) -> u32 {
+    x + 1
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    assert_eq!(giveaway1, ShirtColor::Red);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    assert_eq!(giveaway2, ShirtColor::Blue);
+    println!("The user preference {:?} gets {:?}", user_pref2, giveaway2);
+
+    // annotating closure
+    let expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+    // let _ = expensive_closure(20);
+
+    let add_one_v2 = |x: u32| -> u32 { x + 1 };
+    // let add_one_v3 = |x| { x + 1 };
+    // let add_one_v4 = |x| x + 1;
+
+    // only borrow
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let only_borrows = || println!("From closure: {list:?}");
+
+    println!("Before calling closure: {list:?}");
+    only_borrows();
+    println!("After calling closure: {list:?}");
+
+    // borrow mutably
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let mut borrows_mutably = || list.push(7);
+
+    // println!("After calling closure: {list:?}"); // cannot be done, due to the borrow
+    borrows_mutably();
+    println!("After calling closure: {list:?}");
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    thread::spawn(move || println!("From thread: {list:?}"))
+        .join()
+        .unwrap();
+
+    let mut list = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
+    ];
+    list.sort_by_key(|r| r.width);
+    println!("{list:#?}");
+}
