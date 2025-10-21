eda909117f152f7251beccf8e59cea9a238e219c
Tomas Petricek <tomaspetricek98@gmail.com>
2025-10-21 10:24:39 +0200

    create art library

---
+++ art/src/main.rs
@@ -0,0 +1,8 @@
+use art::{PrimaryColor, mix};
+
+fn main() {
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    let res = mix(&red, &yellow);
+    println!("The result of mixing {red:#?} and {yellow:#?} is: {res:#?}");
+}
