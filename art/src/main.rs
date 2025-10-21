cd31c28dab6dc1794edc7386c7d6f41bcd5e6fa0
Tomas Petricek <tomaspetricek98@gmail.com>
2025-10-21 10:31:28 +0200

    change version

--- art/src/main.rs
+++
@@ -1,8 +0,0 @@
-use art::{PrimaryColor, mix};
-
-fn main() {
-    let red = PrimaryColor::Red;
-    let yellow = PrimaryColor::Yellow;
-    let res = mix(&red, &yellow);
-    println!("The result of mixing {red:#?} and {yellow:#?} is: {res:#?}");
-}
